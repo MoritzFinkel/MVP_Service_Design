15cc193cfe128af46bd85235690ef27f02a1879e
Moritz Finkel <m.finkel@hm.edu>
2026-01-07 21:49:18 +0100

    Remove .open-next folder

--- moosburg-transparent/.open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_antraege_page_actions_1da82aa3.js
+++
@@ -1,3 +0,0 @@
-module.exports=[73832,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_antraege_page_actions_1da82aa3.js.map
--- moosburg-transparent/.open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_antrag_[id]_page_actions_1f2151f8.js
+++
@@ -1,3 +0,0 @@
-module.exports=[3763,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_antrag_%5Bid%5D_page_actions_1f2151f8.js.map
